a676dc7c79353cb4beb50242b2100150fdfb2281
rrbarrostrader <robertorbarros1@gmail.com>
2026-04-11 22:09:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -16,6 +15,16 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "público",
+    emptyOutDir: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
     },
   },
 }));
